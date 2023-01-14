893890f75fdb79b7c573430abb4f708e0d43d71e
NoUmlautsAllowed <58878251+NoUmlautsAllowed@users.noreply.github.com>
2023-01-14 09:47:09 +0100

    Image Cdn: add context write error test
    Signed-off-by: NoUmlautsAllowed <58878251+NoUmlautsAllowed@users.noreply.github.com>

--- pkg/utils/generate.go
+++ pkg/utils/generate.go
@@ -1,3 +1,4 @@
 package utils
 
 //go:generate go run github.com/golang/mock/mockgen -package=utils -destination=mock_httphandler.go net/http Handler
+//go:generate go run github.com/golang/mock/mockgen -package=utils -destination=mock_responsewriter.go github.com/gin-gonic/gin ResponseWriter
