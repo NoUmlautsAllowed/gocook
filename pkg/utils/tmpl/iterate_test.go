c328afd42e68729d81da46ec19dc664ecc01a232
NoUmlautsAllowed <58878251+NoUmlautsAllowed@users.noreply.github.com>
2023-01-26 18:17:48 +0100

    Template Utils: add test for remaining slice length
    Signed-off-by: NoUmlautsAllowed <58878251+NoUmlautsAllowed@users.noreply.github.com>

--- pkg/utils/tmpl/iterate_test.go
+++ pkg/utils/tmpl/iterate_test.go
@@ -38,3 +38,32 @@ func Test_IterateRange(t *testing.T) {
 		}
 	}
 }
+
+func Test_IterateRange2(t *testing.T) {
+	data := []int{
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+	}
+
+	steps := 4
+	out := IterateRange(data, steps)
+
+	expected := int(math.Ceil(float64(len(data)) / float64(steps)))
+
+	if len(out) != expected {
+		t.Error("expected other length, got", len(out), "!=", expected)
+	}
+
+	if len(out[expected-1]) != (len(data) % steps) {
+		t.Error("expected remaining slice with length", len(data)%steps, "got", len(out[expected-1]))
+	}
+}
